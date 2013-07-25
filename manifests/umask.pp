ee19cf9bbcd0b0c22949307320ee80e25c00b629
jacent <jamie@jacent.net>
2013-07-25 11:14:23 +0100

    Update umask.pp
    Validation added to stop additional entries being created per puppet run

--- manifests/umask.pp
+++ manifests/umask.pp
@@ -2,6 +2,7 @@ class hardening::umask {
 
   exec { 'echo "umask 027" >> /etc/sysconfig/init':
     path => "/usr/bin:/usr/sbin:/bin",
+    unless => 'grep "umask 027" /etc/sysconfig/init',
   }
 
 }
